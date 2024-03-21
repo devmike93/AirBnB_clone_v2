798257923abae72db80529cad9b412f78d796f60
Ini <arthurshur@gmail.com>
2024-03-20 17:11:27 -0700

    delete method

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,9 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DATETIME
+from models import storage_type
 
 class BaseModel:
     """A base class for all hbnb models"""
@@ -42,3 +44,9 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
+
+    def delete(self):
+        """deletes the current instance from the storage"""
+        from models import storage
+
+        storage.delete(self)

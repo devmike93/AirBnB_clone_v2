02da3968465d64329ff67f8b35bca36771cc3c64
Michael Megbenu <msk.megbenu@gmail.com>
2024-03-22 03:22:36 +0200

    delete object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,27 +9,23 @@ class FileStorage:
     __objects = {}
 
     def all(self, cls=None):
-        """Returns a dictionary of models currently in storage"""
+        '''
+        Returns a dictionary of models currently in storage
+        return FileStorage.__objects
+        '''
         if cls is None:
             return self.__objects
-        cls_name = cls.__name__
-        dct = {}
-        for key in self.__objects.keys():
-            if key.split('.')[0] == cls_name:
-                dct[key] = self.__objects[key]
-        return dct
-
+        return {k: v for k, v in self.__objects.items() if isinstance(v, cls)}
+    
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.__objects.update(
-            {obj.to_dict()['__class__'] + '.' + obj.id: obj}
-            )
+        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
         """Saves storage dictionary to file"""
-        with open(self.__file_path, 'w') as f:
+        with open(FileStorage.__file_path, 'w') as f:
             temp = {}
-            temp.update(self.__objects)
+            temp.update(FileStorage.__objects)
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
@@ -51,23 +47,20 @@ class FileStorage:
                   }
         try:
             temp = {}
-            with open(self.__file_path, 'r') as f:
+            with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
+                        self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
-
+    
     def delete(self, obj=None):
-        ''' deletes the object obj from the attribute
-            __objects if it's inside it
         '''
-        if obj is None:
-            return
-        obj_key = obj.to_dict()['__class__'] + '.' + obj.id
-        if obj_key in self.__objects.keys():
-            del self.__objects[obj_key]
-
-    def close(self):
-        """Call the reload method"""
-        self.reload()
+        Deletes the object obj from the attribute objects 
+        if it's inside it
+        '''
+        if obj is not None:
+            # Construct the object key
+            obj_key = f"{type(obj).__name__}.{obj.id}"
+            # Remove the object if it exists
+            self.__objects.pop(obj_key, None)
